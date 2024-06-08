95018bdab920cb8215b61f1c431089dd8c5df4e9
Mohammad hadi <m6336.programer@gmail.com>
2024-06-09 00:43:07 +0330

    adde smsir_mh_js

---
+++ smsir_mh_js.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const SendAlertSMSIR = (to, message, api_key, templateId) => {
+  const data = JSON.stringify({
+    "mobile": to,
+    "templateId": templateId,
+    "parameters": [
+      { name: 'CONTACTS', value: message }
+    ],
+  });
+
+  const config = {
+    method: 'post',
+    url: 'https://api.sms.ir/v1/send/verify',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'text/plain',
+      'X-API-KEY': api_key
+    },
+    data: data
+  };
+
+  axios(config)
+    .then(response => {
+      console.info(JSON.stringify(response.data));
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+module.exports = { SendAlertSMSIR };
+
+// Example usage (commented out):
+SendAlertSMSIR("09100059323", "35f389", "your_api_key_here", "750016");
