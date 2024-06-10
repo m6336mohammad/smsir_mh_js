0c8119327d40a845e0a96c1577d64e6095954d3b
Mohammad hadi <m6336.programer@gmail.com>
2024-06-10 06:02:52 +0330

    changed comment

--- testSMS.js
+++ testSMS.js
@@ -1,6 +1,6 @@
 const { SendAlertSMSIR } = require('./smsir_mh_js');
 
-const apiKey = '1212121212121212121';  //Example API Key 
+const apiKey = '1212121212121212121';  //Example API Key get from sms.ir
 const to = '09100059323';  //Example Phone Number
 const message = 'Hello,this test message!'; //Max 24 Character Length
 const templateId = '750016';  //Example template ID 
